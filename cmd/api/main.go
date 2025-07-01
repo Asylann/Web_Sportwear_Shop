4aad9d6e54795e637058a2b02cd588892998a1ca
usena <usenasylan@gmail.com>
2025-07-01 15:37:53 +0500

    Not full completed front-end side!

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -32,6 +32,21 @@ func main() {
 	r := mux.NewRouter()
 	r.Use(middleware.Logging)
 
+	r.Use(func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.Header().Set("Access-Control-Allow-Origin", "*")
+			w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+			w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+			if r.Method == "OPTIONS" {
+				w.WriteHeader(http.StatusOK)
+				return
+			}
+
+			next.ServeHTTP(w, r)
+		})
+	})
+
 	r.HandleFunc("/signup", handlers.CreateUserHandle).Methods("POST")
 	r.HandleFunc("/login", handlers.LoginHandle).Methods("POST")
 
